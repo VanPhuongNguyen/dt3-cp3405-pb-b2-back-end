787074071d198db25d45e5f9003b2afdf2537cd2
Robert Nguyen <robert.nguyen279@gmail.com>
2020-12-24 16:44:07 +0700

    Add prettier

--- src/handler.ts
+++ src/handler.ts
@@ -1,11 +1,11 @@
-import { APIGatewayProxyHandler } from 'aws-lambda';
-import { echo } from '@queries/exampleQuery';
-import 'source-map-support/register';
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { echo } from "@queries/exampleQuery";
+import "source-map-support/register";
 
 export const hello: APIGatewayProxyHandler = async (event) => ({
   statusCode: 200,
   body: JSON.stringify({
-    message: echo('Module aliasing is really the best'),
+    message: echo("Module aliasing is really the aa aaa best"),
     input: event,
   }),
 });
