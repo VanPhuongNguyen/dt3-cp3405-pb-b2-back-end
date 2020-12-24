6fc2f5a96d32dd8540bec151b0cc49efb76d17ac
Robert Nguyen <robert.nguyen279@gmail.com>
2020-12-24 17:18:36 +0700

    Add prettier

--- src/handler.ts
+++ src/handler.ts
@@ -5,7 +5,7 @@ import 'source-map-support/register';
 export const hello: APIGatewayProxyHandler = async (event, context) => ({
   statusCode: 200,
   body: JSON.stringify({
-    message: echo('Module aliasing is really the best'),
+    message: echo('Module aliasing is really the a a a best'),
     input: event,
   }),
 });
