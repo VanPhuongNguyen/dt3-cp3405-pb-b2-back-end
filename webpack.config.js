572957c1688b72edcd48bc688dc6b9f4f2584dea
Robert Nguyen <robert.nguyen279@gmail.com>
2020-12-25 10:29:49 +0700

    Fix alias

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 const path = require('path');
 const slsw = require('serverless-webpack');
 const nodeExternals = require('webpack-node-externals');
-// const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   context: __dirname,
@@ -17,7 +17,6 @@ module.exports = {
     cacheWithContext: false,
     alias: {
       '@src': path.resolve(__dirname, './src'),
-      '@queries': path.resolve(__dirname, './queries'),
       '@tests': path.resolve(__dirname, './tests'),
     },
   },
@@ -52,11 +51,8 @@ module.exports = {
     ],
   },
   plugins: [
-    // new ForkTsCheckerWebpackPlugin({
-    //   eslint: true,
-    //   eslintOptions: {
-    //     cache: true
-    //   }
-    // })
+    new ForkTsCheckerWebpackPlugin({
+      eslint: true,
+    }),
   ],
 };
